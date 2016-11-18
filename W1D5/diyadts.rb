3c5dd20351b7d2096d7797909b60f452375b1063
Hope <hopewanroy@gmail.com>
2016-11-17 23:32:25 -0800

    Add Stack, Queue, and Map classes

---
+++ W1D5/diyadts.rb
@@ -0,0 +1,112 @@
+# EXERCISE 1 - STACK (LIFO)
+
+class Stack
+
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+
+end
+
+stack = Stack.new
+stack.add(4)
+stack.add(5)
+stack.add(10)
+p stack.show
+stack.remove
+stack.remove
+p stack.show
+
+
+# EXERCISE 2 - QUEUE (FIFO)
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue
+  end
+
+end
+
+queue = Queue.new
+queue.enqueue(4)
+queue.enqueue(5)
+queue.enqueue(10)
+p queue.show
+queue.dequeue
+queue.dequeue
+p queue.show
+
+# EXERCISE 3 - MAP (Array Implementation)
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    @map.each do |pair|
+      if pair[0] == key
+        pair = [key, value]
+      end
+    end
+    @map << [key, value]
+  end
+
+  def lookup(key)
+    @map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
+    nil
+  end
+
+  def remove(key)
+    @map.each_with_index do |pair, index|
+      if pair.first == key
+        @map.delete_at(index)
+      end
+    end
+  end
+
+  def show
+    @map
+  end
+
+end
+
+map = Map.new
+map.assign(4, "I")
+map.assign(5, "LOVE")
+map.assign(10, "YOU")
+p map.show
+map.assign(10, "Richard")
+p map.show
+p map.lookup(10)
+map.remove(4)
+map.remove(10)
+p map.show
