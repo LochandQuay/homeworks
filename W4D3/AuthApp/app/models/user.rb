8393015da131658d30a37d589dc8f7482ef14268
Hope <hopewanroy@gmail.com>
2016-12-06 22:35:46 -0800

    Add validations to User model

--- W4D3/AuthApp/app/models/user.rb
+++ W4D3/AuthApp/app/models/user.rb
@@ -1,2 +1,43 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
+  before_validation
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+
+  def self.find_by_credentials(username, password)
+
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token
+  end
+
+  def ensure_session_token
+
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
