9e56e7e8a4188f797512e36f09b10657114906d9
Hope <hopewanroy@gmail.com>
2016-12-07 22:52:26 -0800

    Add 99 Cats

--- W4D3/AuthApp/app/models/user.rb
+++ W4D3/AuthApp/app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
     user = User.find_by(username: username)
     return nil if user.nil?
     user.is_password?(password) ? user : nil
-    # return user if user && BCrypt::Password.new(password_digest).is_password?(password)
+    # return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
     # nil
   end
 
