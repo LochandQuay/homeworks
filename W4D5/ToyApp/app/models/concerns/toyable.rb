05ba7f6a89f2181f0f527fb7d5f5da27a2fd295a
Hope <hopewanroy@gmail.com>
2016-12-08 19:25:23 -0800

    Include Toyable module in Cat and Corgi models

--- W4D5/ToyApp/app/models/concerns/toyable.rb
+++ W4D5/ToyApp/app/models/concerns/toyable.rb
@@ -1,11 +1,12 @@
 module Toyable
   extend ActiveSupport::Concern
 
-  include do
+  included do
     has_many :toys, as: :toyable
     # validates :toys
   end
 
   def receive_toy(name)
+    self.toys << Toy.find_or_create_by(name: name)
   end
 end
