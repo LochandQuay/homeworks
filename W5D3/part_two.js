8d1227bd1c00f038be899bb0931050864f4897db
Hope <hopewanroy@gmail.com>
2016-12-14 00:47:31 -0800

    Add Phase II: Constructors, Prototypes, and this

--- W5D3/part_two.js
+++ W5D3/part_two.js
@@ -15,3 +15,27 @@ function titleize(names, callback) {
 }
 
 // Phase II: Constructors, Prototypes, and this
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function() {
+  return `${this.name} the elephant goes "phrRRRRRRRRRR!!!!!"`;
+}
+
+Elephant.prototype.grow = function() {
+  return this.height += 12;
+}
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+  return this.tricks;
+}
+
+Elephant.prototype.play = function() {
+  var max = this.tricks.length;
+  var randomNumber = Math.floor(Math.random() * (max + 1));
+  return `${this.name} is ${this.tricks[randomNumber]}!`;
+}
