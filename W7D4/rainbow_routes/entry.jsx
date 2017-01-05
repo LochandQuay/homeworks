d1de3f220dd965371fceb780b44db7d0b75bdf7c
Hope <hopewanroy@gmail.com>
2017-01-04 17:06:18 -0800

    Add nested routes and hash history for onClicks

--- W7D4/rainbow_routes/entry.jsx
+++ W7D4/rainbow_routes/entry.jsx
@@ -58,9 +58,14 @@ Rainbow = withRouter(Rainbow);
 
 const routes = (
   <Route path="/" component={Rainbow}>
-      <Route path="red" component={Red} />
+      <Route path="red" component={Red}>
+        <Route path="orange" component={Orange} />
+        <Route path="yellow" component={Yellow} />
+      </Route>
       <Route path="green" component={Green} />
-      <Route path="blue" component={Blue} />
+      <Route path="blue" component={Blue}>
+        <Route path="indigo" component={Indigo} />
+      </Route>
       <Route path="violet" component={Violet} />
   </Route>
 );
